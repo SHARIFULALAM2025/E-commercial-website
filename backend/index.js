ae0a72680c770f16549c19ac6c450f0a3a7b934a
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2025-12-12 12:39:20 +0600

    first commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 5000
+require('dotenv').config()
+app.use(express.json())
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send('e-commers-website server is running. .......')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ frontend/my-website/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center'>website</h1>
+    </>
+  )
+}
+
+export default App
